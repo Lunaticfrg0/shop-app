517fe483d95136cac566bb3b59d59a374b9eae90
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-27 17:48:59 -0400

    Edit shop files to allow pug template usage

--- src/routers/shop.js
+++ src/routers/shop.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = new express.Router()
 const path = require('path')
 
+const adminData = require('./admin')
+
 const rootDir = require('../utils/path')
 
 
 router.get('/',(req, res, next ) => {
-    res.sendFile(path.join(rootDir ,'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {prods: products, docTitle: 'E-Shop'})
 })
 
 module.exports = router
