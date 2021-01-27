cb53d3178888b4b05f9a0d6f08d76a12709fcb7f
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-27 18:31:01 -0400

    change endpoints to pass the correct information

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -7,8 +7,7 @@ const rootDir = require('../utils/path')
 const products = []
 
 router.get('/add-product',(req, res, next ) => {
-
-    res.sendFile(path.join(rootDir ,'views', 'add-product.html'))
+    res.render('add-product', {pageTitle: "Add Product", path : "/admin/add-product"})
 })
 router.post('/add-product',(req, res, next ) => {
     products.push({title: req.body.title})
