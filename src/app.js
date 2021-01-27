d1cf4eab5211ab854baf98b9cc62991f60184173
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-27 17:48:27 -0400

    change main structure to allow template engine usage

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const path = require('path')
 
-const adminRoutes = require('./routers/admin')
+const adminData = require('./routers/admin')
 const shopRoutes = require('./routers/shop')
 
 const port = 3000
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, './', "views"))
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/admin',adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next)=> {
