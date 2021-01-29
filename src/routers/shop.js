1362b409dead94a16ba8a0f265f2d85e9b11e088
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-29 19:43:28 -0400

    make changes to allow functionality

--- src/routers/shop.js
+++ src/routers/shop.js
@@ -5,11 +5,18 @@ const path = require('path')
 const adminData = require('./admin')
 
 const rootDir = require('../utils/path')
+const { toUpperCase } = require('../utils/path')
 
 
 router.get('/',(req, res, next ) => {
     const products = adminData.products
-    res.render('shop', {prods: products, pageTitle: "e-Shop", path: "/"})
+    res.render('shop', 
+    {prods: products, 
+    pageTitle: "e-Shop", 
+    path: "/", 
+    hasProducts: products.length > 0, 
+    activeShop: true,
+    productCSS : true})
 })
 
 module.exports = router
