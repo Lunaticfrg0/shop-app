69ec813bd7e8e8f4774cd968aeb366e81ca01a79
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-30 19:37:20 -0400

    Modify routers to allow support of new view

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -27,6 +27,14 @@ exports.getCart= (req, res, next) =>{
     path: "/cart", 
     })
 }
+
+exports.getOrders= (req, res, next) =>{
+    res.render('shop/orders', { 
+    pageTitle: "Your Orders", 
+    path: "/orders", 
+    })
+}
+
 exports.getCheckout= (req, res, next) =>{
     res.render('shop/checkout', { 
     pageTitle: "Checkout", 
