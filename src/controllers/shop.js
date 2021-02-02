384255f8618915a613a6f2aba20658592173138c
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-02 18:41:20 -0400

    create endpoint to handle product detail

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product')
 
-
 exports.getProducts = (req, res, next ) => {
     Product.fetchAll((products)=> {
         res.render('shop/product-list', 
@@ -11,10 +10,22 @@ exports.getProducts = (req, res, next ) => {
     })
 }
 
+exports.getProduct = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        res.render('shop/product-detail', {
+          product: product,
+          pageTitle: product.title,
+          path: '/products'
+        });
+      });
+}
+
 exports.getIndex = (req, res, next) =>{
     Product.fetchAll((products)=> {
         res.render('shop/index', 
-        {prods: products, 
+        {
+        prods: products, 
         pageTitle: "e-Shop", 
         path: "/", 
         })
