962d858de092bf006d1dbbf7f683da99f1bcb14d
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-08 18:27:47 -0400

    add edit page controller and  adjust add page controller

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -2,21 +2,31 @@ const Product = require('../models/product')
 
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', 
-    {pageTitle: "Add Product", 
-    path : "/admin/add-product", 
-    formsCSS: true, 
-    productCSS: true,
-    activeAddProduct: true})
+    res.render('admin/edit-product', {
+        pageTitle: "Add Product", 
+        path : "/admin/add-product", 
+    })
 }
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products)=> {
-        res.render('admin/products', 
-        {prods: products, 
-        pageTitle: "Admin Products", 
-        path: "/admin/products", 
+        res.render('admin/products', {
+            prods: products, 
+            pageTitle: "Admin Products", 
+            path: "/admin/products", 
         })
+    })
+}
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+    if(!editMode){
+        return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+        pageTitle: "Edit Product", 
+        path : "/admin/edit-product",
+        editing: editMode,
+
     })
 }
 
