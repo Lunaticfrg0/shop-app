82ded1da02bda524f4f9441ac2ec5d011c280e15
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-08 18:07:48 -0400

    update controller to hold cart functionality

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product')
+const Cart = require('../models/cart')
+
 
 exports.getProducts = (req, res, next ) => {
     Product.fetchAll((products)=> {
@@ -34,7 +36,9 @@ exports.getIndex = (req, res, next) =>{
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId
-  console.log(productId)
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price)
+  })
   res.redirect('/cart')
 }
 
