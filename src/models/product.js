3a2efbccf9b4c36433e72d5efec05ced43662bc2
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-09 18:58:49 -0400

    update models to allow deletion of products

--- src/models/product.js
+++ src/models/product.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const pathUtil = require('../utils/path')
 
+const Cart = require('./cart')
+
 const p = path.join(pathUtil, 
     'data', 
     'products.json')
@@ -44,6 +46,17 @@ module.exports = class Product {
             }
         })
        
+    }
+    static deleteById(id){
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id)
+            const updatedProducts = products.filter((product)=> product.id !== id) 
+            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                if(!err){
+                    Cart.deleteProduct(id, product.price)
+                }
+            })
+        })
     }
     static fetchAll(cb){
         getProductsFromFile(cb)
