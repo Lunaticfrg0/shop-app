17b7d54c10363da2be02651196d809fb7eefe3c9
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-09 19:43:21 -0400

    add get cart controller and change cart ejs file

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,7 +1,6 @@
 const Product = require('../models/product')
 const Cart = require('../models/cart')
 
-
 exports.getProducts = (req, res, next ) => {
     Product.fetchAll((products)=> {
         res.render('shop/product-list', { 
@@ -42,16 +41,28 @@ exports.postCart = (req, res, next) => {
 }
 
 exports.getCart= (req, res, next) =>{
-    res.render('shop/cart', { 
-        pageTitle: "Your Cart", 
-        path: "/cart", 
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = []
+            for(product of products){
+                const cartProductData = cart.products.find(prod => prod.id === product.id)
+                if(cartProductData){
+                    cartProducts.push({productData: product, qty: cartProductData.qty})
+                }
+           }
+           res.render('shop/cart', { 
+                pageTitle: "Your Cart", 
+                path: "/cart",
+                products: cartProducts
+            })
+        })
     })
 }
 
 exports.getOrders= (req, res, next) =>{
     res.render('shop/orders', { 
         pageTitle: "Your Orders", 
-        path: "/orders", 
+        path: "/orders",
     })
 }
 
