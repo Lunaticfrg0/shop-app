c43d54b9ad6fb33142aad57fc9be141691c94144
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-17 19:15:15 -0400

    configure support of mysql usage, with defaults of tables

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const sequelize = require('./utils/database');
 const adminRoutes = require('./routers/admin');
 const shopRoutes = require('./routers/shop');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const errorCnontroller = require('./controllers/error');
 
 const port = 3000;
@@ -18,14 +23,46 @@ app.set('views', path.join(__dirname, './', 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorCnontroller.get404);
 
-sequelize
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+User.sequelize
+  //.sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: 'Emilio',
+        email: 'emilio3scano@gmail.com'
+      });
+    }
+    return user;
+  })
+  .then((user) => {
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(port, () => {
       console.log('Server is up on port ' + port);
     });
