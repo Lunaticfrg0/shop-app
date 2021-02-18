b4a7e33bcb9d4b91372c4bddd411128fabd9246e
Lunaticfrg0 <emilio3scano@gmail.com>
2021-02-17 20:06:42 -0400

    modify route and controller

--- src/routers/shop.js
+++ src/routers/shop.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const router = new express.Router()
-const path = require('path')
+const express = require('express');
+const router = new express.Router();
+const path = require('path');
 
-const shopController = require('../controllers/shop')
+const shopController = require('../controllers/shop');
 
-router.get('/', shopController.getIndex)
+router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts)
+router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart)
-router.post('/cart', shopController.postCart)
-router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders)
+router.post('/create-order', shopController.postOrder);
+router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout)
+router.get('/checkout', shopController.getCheckout);
 
-module.exports = router
+module.exports = router;
